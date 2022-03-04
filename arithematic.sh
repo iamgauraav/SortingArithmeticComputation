e75e3a569fd3983247b1ecdde7e58d59fe75080e
iamgauraav <iamgauraav@gmail.com>
2022-03-04 12:45:41 +0530

    Adding UC6 : code to store computation in dictionary

--- arithematic.sh
+++ arithematic.sh
@@ -4,8 +4,17 @@ read -p "Enter a number A :- " a
 read -p "Enter a number B :- " b
 read -p "Enter a number C :- " c
 
-result=$(($a % $b + $c))
+declare -A dictionary
 
-echo "$result"
+result1=$(($a + $b * $c))
+result2=$(($a * $b + $c))
+result3=$(($c + $a / $b))
+result4=$(($a % $b + $c))
 
+dictionary[value1]=$result1
+dictionary[value2]=$result2
+dictionary[value3]=$result3
+dictionary[value4]=$result4
+
+echo ${dictionary[@]}
 
