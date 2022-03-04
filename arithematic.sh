267b882ab244901441f91e5353bb3bd4ce0a32a9
iamgauraav <iamgauraav@gmail.com>
2022-03-04 12:52:16 +0530

    Adding UC7 : code to store computation in array

--- arithematic.sh
+++ arithematic.sh
@@ -5,6 +5,7 @@ read -p "Enter a number B :- " b
 read -p "Enter a number C :- " c
 
 declare -A dictionary
+array=();
 
 result1=$(($a + $b * $c))
 result2=$(($a * $b + $c))
@@ -16,5 +17,5 @@ dictionary[value2]=$result2
 dictionary[value3]=$result3
 dictionary[value4]=$result4
 
-echo ${dictionary[@]}
-
+array+=${dictionary[@]}
+echo ${array[@]}
